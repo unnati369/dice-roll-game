01394ea609cc431d1deb963e769d5f11e761a97f
Unnati Makrariya <90144196+unnati369@users.noreply.github.com>
2022-11-05 21:15:32 +0530

    fix: output display bug fixed

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 var btn = document.querySelector("#btn")
-var output = document.querySelector("#output") 
-var statement = document.querySelector("#statement") 
-var img1 = document.querySelector("#dice1") 
-var img2 = document.querySelector("#dice2") 
-btn.addEventListener("click",()=>{
-    var array =[1,2,3,4,5,6]
-    var randomno1 = Math.floor(Math.random()*6)
-    var randomno2 = Math.floor(Math.random()*6)
+var output = document.querySelector("#output")
+var statement = document.querySelector("#statement")
+var img1 = document.querySelector("#dice1")
+var img2 = document.querySelector("#dice2")
+btn.addEventListener("click", () => {
+    var array = [1, 2, 3, 4, 5, 6]
+    var randomno1 = Math.floor(Math.random() * 6)
+    var randomno2 = Math.floor(Math.random() * 6)
 
-    img1.src = "./images/"+ `${Number(randomno1)+1}.jpeg`
-    img2.src = "./images/"+ `${Number(randomno2)+1}.jpeg`
+    img1.src = "./images/" + `${Number(randomno1)+1}.jpeg`
+    img2.src = "./images/" + `${Number(randomno2)+1}.jpeg`
 
-    if(randomno1 > randomno2){
-statement.innerHTML = "Player One wins🥇"
+    if (randomno1 > randomno2) {
+        statement.style.display = "block"
+        statement.innerHTML = "Player One wins🥇"
+
+    } else if (randomno1 < randomno2) {
+        statement.style.display = "block"
+        statement.innerHTML = "Player Two wins🥇"
+
+    } else if (randomno1 === randomno2) {
+        statement.style.display = "block"
+        statement.innerHTML = "tie😁"
 
     }
-   else if(randomno1 < randomno2){
-        statement.innerHTML = "Player Two wins🥇"
-        
-            }
-            else if(randomno1 === randomno2){
-                statement.innerHTML = "tie😁"
-                
-                    }
-    
+
 
 })
